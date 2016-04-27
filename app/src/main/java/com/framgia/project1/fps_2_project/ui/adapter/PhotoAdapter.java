7d8c4eac87217b1b505162558d03358187ee1896
nguyenquang1995 <hackspeed1995@gmail.com>
2016-04-27 08:56:33 +0700

    choose multi image

---
+++ app/src/main/java/com/framgia/project1/fps_2_project/ui/adapter/PhotoAdapter.java
@@ -0,0 +1,97 @@
+package com.framgia.project1.fps_2_project.ui.adapter;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+
+import com.framgia.project1.fps_2_project.R;
+import com.framgia.project1.fps_2_project.data.model.PhotoModel;
+
+import java.util.ArrayList;
+
+/**
+ * Created by hacks_000 on 4/13/2016.
+ */
+public class PhotoAdapter extends BaseAdapter {
+    private LayoutInflater mInflater;
+    private Context mContext;
+    private ArrayList<PhotoModel> mListImages;
+
+    public PhotoAdapter(Context context, ArrayList<PhotoModel> listImages) {
+        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mContext = context;
+        mListImages = listImages;
+    }
+
+    @Override
+    public int getCount() {
+        return mListImages.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final MyViewHolder mMyViewHolder;
+        if (convertView == null) {
+            mMyViewHolder = new MyViewHolder();
+            convertView = mInflater.inflate(R.layout.choose_photo_item, null, false);
+            mMyViewHolder.mImgThumb = (ImageView) convertView.findViewById(R.id.img_photo);
+            mMyViewHolder.mCheckBox = (CheckBox) convertView.findViewById(R.id.checkbox_photo);
+            convertView.setTag(mMyViewHolder);
+        } else {
+            mMyViewHolder = (MyViewHolder) convertView.getTag();
+        }
+        mMyViewHolder.mImgThumb.setId(position);
+        mMyViewHolder.mCheckBox.setId(position);
+        mMyViewHolder.mCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CheckBox checkBox = (CheckBox) v;
+                int position = checkBox.getId();
+                if (mListImages.get(position).isSelected()) {
+                    checkBox.setChecked(false);
+                    mListImages.get(position).setSelected(false);
+                } else {
+                    checkBox.setChecked(true);
+                    mListImages.get(position).setSelected(true);
+                }
+            }
+        });
+        mMyViewHolder.mImgThumb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = mMyViewHolder.mCheckBox.getId();
+                if (mListImages.get(position).isSelected()) {
+                    mMyViewHolder.mCheckBox.setChecked(false);
+                    mListImages.get(position).setSelected(false);
+                } else {
+                    mMyViewHolder.mCheckBox.setChecked(true);
+                    mListImages.get(position).setSelected(true);
+                }
+            }
+        });
+        mMyViewHolder.mImgThumb.setImageBitmap(mListImages.get(position).getBitmap());
+        mMyViewHolder.mCheckBox.setChecked(mListImages.get(position).isSelected());
+        return convertView;
+    }
+    private class MyViewHolder {
+        private ImageView mImgThumb;
+        private CheckBox mCheckBox;
+    }
+}
